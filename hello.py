afcf305259ee997e3259c71f905874903ae69057
izchay007 <iz_chay@hotmail.com>
2024-01-14 23:34:11 +0800

    button disabled and size padx

--- hello.py
+++ hello.py
@@ -2,10 +2,13 @@ from tkinter import*
 
 root = Tk()
 
-myLabel1 = Label(root, text="Hello World!")
-myLabel2 = Label(root, text="word")
+#myButton = Button(root, text="click Me!", state=DISABLED)
+myButton = Button(root, text="click Me!", padx=50)
+myButton.pack()
+#myLabel1 = Label(root, text="Hello World!").grid(row=0,column=0)
+#myLabel2 = Label(root, text="my name is isaac").grid(row=1,column=5)
 
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=10)
+#myLabel1.grid(row=0, column=0)
+#myLabel2.grid(row=1, column=10)
 
 root.mainloop()
