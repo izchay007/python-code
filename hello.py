945f2f35824d727035437b14650b57bfe68c1305
izchay007 <iz_chay@hotmail.com>
2024-01-14 23:41:36 +0800

    button click function Label

--- hello.py
+++ hello.py
@@ -2,8 +2,12 @@ from tkinter import*
 
 root = Tk()
 
-#myButton = Button(root, text="click Me!", state=DISABLED)
-myButton = Button(root, text="click Me!", padx=50)
+def myClick():
+    myLabel = Label(root, text="Look! I clicked a button!!")
+    myLabel.pack()
+
+#myButton = Button(root, text="click Me!", state=DISABLED,padx=50, pady=50)
+myButton = Button(root, text="click Me!",command =myClick)
 myButton.pack()
 #myLabel1 = Label(root, text="Hello World!").grid(row=0,column=0)
 #myLabel2 = Label(root, text="my name is isaac").grid(row=1,column=5)
