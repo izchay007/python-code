36485ae9667d3fb03b77021eacd88ec11480e8e4
izchay007 <iz_chay@hotmail.com>
2024-01-19 01:06:34 +0800

    update math for equal

--- calculator.py
+++ calculator.py
@@ -19,22 +19,51 @@ def button_clear():
 def button_add():
     first_number = e.get()
     global f_num
+    global math
+    math = "addition"
     f_num = int(first_number)
     e.delete(0,END)
 
 def button_equal():
     second_number = e.get()
     e.delete(0,END)
-    e.insert(0, f_num + int(second_number))
+
+    if math == "addition":
+        e.insert(0, f_num + int(second_number))
+
+    if math == "minus":
+        e.insert(0, f_num - int(second_number))
+
+    if math == "multiplication":
+        e.insert(0, f_num * int(second_number))
+    
+    if math == "division":
+        e.insert(0, f_num / int(second_number))
+
 
 def button_minus():
-    return
+    first_number = e.get()
+    global f_num
+    global math
+    math = "minus"
+    f_num = int(first_number)
+    e.delete(0,END)
 
 def button_multiply():
-    return
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0,END)
 
 def button_divide():
-    return
+    first_number = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0,END)
 
 #Define buttons
 button1 = Button(root,text="1", padx=40, pady=20, command=lambda: button_click(1))
