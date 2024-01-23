8d80c0484b81478b81c37a0e2021de37eec57391
izchay007 <iz_chay@hotmail.com>
2024-01-24 00:25:31 +0800

    rw file close append

--- rw file.py
+++ rw file.py
@@ -1,3 +1,30 @@
 # Open function to open the file "MyFile1.txt" (same directory) in append mode and
-file1 = open("MyFile1.txt","a")
+#file1 = open("MyFile1.txt","a")
+#file1.close()
+file1 = open("MyFile1.txt","w+")
+
+#write a string to file MyFile1.txt
+#s = "Hello\n"
+#file1.write(s)
+
+#file1.close()
+
+file1 = open("MyFile1.txt","r")
+print(file1.read())
 file1.close()
+
+file1 = open("MyFile1.txt","w")
+L = ["This is sparta\n", "This is richard\n"]
+file1.writelines(L)
+file1.close()
+
+file1 = open("MyFile1.txt", "a") #append mode
+file1.write("Hello\n")  
+file1.close()
+
+file1 = open("MyFile1.txt", "r") #read mode
+print("Output before append")
+print(file1.read())
+print()
+file1.close()
+
